f955a7ed7f63564841a3d6b94a5d99243c1216f9
metalkhan <14113104@student.unza.zm>
2020-12-13 20:31:10 +0200

    completed header

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+
+
+
+?>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <div class="jumbotron jumbotron-fluid" style="background: rgb(70, 182, 211);">
+            <div class="row justify-content-md-end justify-content-sm-end">
+    <h5 class="px-5 display-5">You are logged in as <?php  ?></h5>
+            </div>
+        <div class="container">
+            <h1 class="display-4">ICT 1110 Computer System and Architecture (2019/20)</h1>
+            <h3 class="display-4">Makeup Requesting Platform</h3>
+    </div>
+
+    </div>
